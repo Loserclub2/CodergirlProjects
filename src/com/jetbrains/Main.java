6693651b6553cc93eb691ec5bddd7ac18bea2d6b
Loserclub2 <46978080+Loserclub2@users.noreply.github.com>
2019-03-03 22:41:17 -0600

    Add files via upload

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,89 @@
+package com.jetbrains;
+
+ import static java.lang.System. out;
+ import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner (System.in);
+
+
+        int goalForVeggies = 50;
+        int goalForBurgers = 250;
+        int goalForSubs = 180;
+        int goalForSoups = 70;
+
+        out.println("Checking sales goals for today.");
+        out.println("The sales goal for veggies sandwhiches is 50");
+        out.println("How many veggies sandwhiches were sold?");
+        keyboard.nextInt();
+
+        if  (goalForVeggies > == 50){
+            out.println("Made goal");
+
+        } else {
+
+            out.println("Fell short");
+
+        }
+
+        out.println("The sales goal for burgers is 250");
+        out.println("How many burgers were sold?");
+        keyboard.nextInt();
+
+        if (goalForBurgers >= 250) {
+            out.println("Made goal");
+
+        } else {
+
+            out.println("Fell short");
+
+        }
+            out.println("The sales goal for sub sandwiches is 180");
+            out.println("How many sub sandwhiches were sold?");
+            keyboard.nextInt();
+
+
+            if (goalForSubs >= 180) {
+                out.println("Made goal");
+
+            } else {
+
+                out.println("Fell short");
+
+            }
+
+        out.println("The sales goal for soup is 70");
+        out.println("How many soups were sold?");
+        keyboard.nextInt();
+
+        if (goalForSoups >= 70) {
+            out.println("Made goal");
+
+        } else {
+
+            out.println("Fell short");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
